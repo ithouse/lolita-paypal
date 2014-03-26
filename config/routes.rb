31ba495a332f95b2d78055145aeb7e46f6aa38ce
Gatis Tomsons <gatis@ithouse.cc>
2014-03-26 14:42:36 +0200

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/paypal/checkout" => "lolita_paypal/transactions#checkout", as: "checkout_paypal"
-  post "/paypal/answer" => "lolita_paypal/transactions#answer", as: "answer_paypal"
+  get "/paypal/answer" => "lolita_paypal/transactions#answer", as: "answer_paypal"
+  post "/paypal/answer" => "lolita_paypal/transactions#answer"
 end
